7d31754888eda370d887193f7200596ef159d787
novhawk37-admin <novhawk37@gmail.com>
2026-07-14 23:39:49 +0530

    Update client.js

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,13 +1,12 @@
 import axios from 'axios'
 
-const BASE_URL = 'http://192.168.29.76:8001'
+const BASE_URL = import.meta.env.VITE_API_URL
 
 const client = axios.create({
   baseURL: BASE_URL,
   headers: {
     'Content-Type': 'application/json',
   },
-
   timeout: 10000,
 })
 
