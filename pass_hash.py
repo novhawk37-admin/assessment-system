3b978c44ada58844e1905c159539baeedd863442
novhawk37-admin <novhawk37@gmail.com>
2026-07-20 21:17:14 +0530

    Delete pass_hash.py

--- pass_hash.py
+++
@@ -1,8 +0,0 @@
-from passlib.context import CryptContext
-
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-
-password = "user33"
-hashed = pwd_context.hash(password)
-
-print(hashed)
